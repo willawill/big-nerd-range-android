ecaa25e43383bb774fd04ba72998372cafc494c2
Willa Wang <willa@neo.com>
2014-11-23 12:51:20 -0500

    Added next button.

--- HistoryQuiz/src/com/willa/historyquiz/QuizActivity.java
+++ HistoryQuiz/src/com/willa/historyquiz/QuizActivity.java
@@ -1,13 +1,16 @@
 package com.willa.historyquiz;
+
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
 public class QuizActivity extends ActionBarActivity {
 	private Button mTrueButton;
 	private Button mFalseButton;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -15,18 +18,21 @@ public class QuizActivity extends ActionBarActivity {
 		mTrueButton = (Button) findViewById(R.id.true_button);
 		mFalseButton = (Button) findViewById(R.id.false_button);
 		mTrueButton.setOnClickListener(new View.OnClickListener() {
-		@Override
-		public void onClick(View v) {
-		Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
-		}
-		});
-		mFalseButton.setOnClickListener(new View.OnClickListener() {
-		@Override
-		public void onClick(View v) {
-		Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(QuizActivity.this, R.string.correct_toast,
+						Toast.LENGTH_SHORT).show();
 			}
 		});
-		}
+		mFalseButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(QuizActivity.this, R.string.incorrect_toast,
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
---
+++ HistoryQuiz/src/com/willa/historyquiz/TrueFalse.java
@@ -0,0 +1,29 @@
+package com.willa.historyquiz;
+
+public class TrueFalse {
+	private int mQuestion;
+	private boolean mTrueQuestion;
+	
+	public TrueFalse(int question, boolean trueQuestion){
+		this.mQuestion = question;
+		this.mTrueQuestion = trueQuestion;
+	}
+
+	public int getQuestion() {
+		return mQuestion;
+	}
+
+	public void setQuestion(int question) {
+		mQuestion = question;
+	}
+
+	public boolean isTrueQuestion() {
+		return mTrueQuestion;
+	}
+
+	public void setTrueQuestion(boolean trueQuestion) {
+		mTrueQuestion = trueQuestion;
+	}
+	
+
+}
