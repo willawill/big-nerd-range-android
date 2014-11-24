c7d5f47fff1699b53ce219a2cee2f0482526e4b1
Willa Wang <willa@neo.com>
2014-11-23 22:03:28 -0500

    Added CheckAnswer. P20 - P40

--- HistoryQuiz/src/com/willa/historyquiz/QuizActivity.java
+++ HistoryQuiz/src/com/willa/historyquiz/QuizActivity.java
@@ -5,11 +5,18 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends ActionBarActivity {
 	private Button mTrueButton;
 	private Button mFalseButton;
+	private Button mNextButton;
+	private TextView mQuestionView;
+	private TrueFalse[] mQuestionBank = new TrueFalse[] {
+			new TrueFalse(R.string.question_china, true),
+			new TrueFalse(R.string.question_america, false) };
+	private int mCurrentIndex = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,18 +24,30 @@ public class QuizActivity extends ActionBarActivity {
 		setContentView(R.layout.activity_quiz);
 		mTrueButton = (Button) findViewById(R.id.true_button);
 		mFalseButton = (Button) findViewById(R.id.false_button);
+		mNextButton = (Button) findViewById(R.id.next_button);
+
+		mQuestionView = (TextView) findViewById(R.id.question_text_view);
+		setQuestion();
+
 		mTrueButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(QuizActivity.this, R.string.correct_toast,
-						Toast.LENGTH_SHORT).show();
+				checkAnswer(true);
 			}
 		});
+
 		mFalseButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(QuizActivity.this, R.string.incorrect_toast,
-						Toast.LENGTH_SHORT).show();
+				checkAnswer(false);
+			}
+		});
+
+		mNextButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				setQuestion();
 			}
 		});
 	}
@@ -38,5 +57,22 @@ public class QuizActivity extends ActionBarActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.quiz, menu);
 		return true;
+	}
+
+	private void setQuestion(){
+		int question = mQuestionBank[this.mCurrentIndex].getQuestion();
+		mQuestionView.setText(question);
+	}
+
+	private void checkAnswer(boolean userInput){
+		boolean answerIsTrue = this.mQuestionBank[mCurrentIndex].isTrueQuestion();
+		int messageId;
+		if ( userInput == answerIsTrue ){
+			messageId = R.string.correct_toast;
+		}
+		else{
+			messageId = R.string.incorrect_toast;
+		}
+		Toast.makeText(QuizActivity.this, messageId, Toast.LENGTH_SHORT).show();
 	}
 }
